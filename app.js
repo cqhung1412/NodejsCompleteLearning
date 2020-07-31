9f71a7ba2ec262f9a6603b59ca6e4c501a9583b8
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-08-01 00:40:37 +0700

    30. Sending Responses

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const http = require('http');
 
-function reqListener(request, response) {
-  console.log(request);
-}
-
-const server = http.createServer(reqListener);
+const server = http.createServer((req, res) => {
+  // console.log(req.url, '\n', req.method, '\n', req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Nodejs Page</title></head>')
+  res.write('<body><h1>Hello Nodejs</h1></body>')
+  res.write('</html>')
+  res.end()
+  res.write()
+  // process.exit();
+});
 
 server.listen(6900)
 
