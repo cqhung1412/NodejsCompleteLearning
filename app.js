666d067b0b90065a5ebd158e1b05f354903f9922
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-08-01 00:52:29 +0700

    32. Routing Requests

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  // console.log(req.url, '\n', req.method, '\n', req.headers);
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Your Message</title></head>');
+    res.write(`<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></body>`);
+    res.write('</html>');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Nodejs Page</title></head>')
-  res.write('<body><h1>Hello Nodejs</h1></body>')
-  res.write('</html>')
-  res.end()
-  res.write()
-  // process.exit();
+  res.write('<head><title>My First Nodejs Page</title></head>');
+  res.write(`<body><h1>Hello Nodejs, Welcome to \"${url}\"</h1></body>`);
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(6900)
