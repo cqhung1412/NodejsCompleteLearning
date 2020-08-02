a5ef0a24156a1eec1f1bb35a58094e6970bf1b70
Bear <wokhug99@gmail.com>
2020-08-02 20:19:03 +0700

    62. Express.js - Looking Behind the Scenes

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
-const http = require('http');
-
 const express = require('express');
-
-// const routes = require('./routes');
 
 const app = express()
 
 app.use((req, res, next) => {
   console.log('Inside middleware 1');
-  res.write(`
-    <html>
-      <head></head>
-  `);
   next(); // Allows the req come to below middleware
 });
 
 app.use((req, res, next) => {
   console.log('Inside middleware 2');
-  res.write(`
-      <body>
-        <h1>Hello Expressjs</h1>
-      </body>
-    </html>
-  `);
+  res.send('<h1>Love from Express</h1>');
   next();
 });
 
-const server = http.createServer(app);
-
-server.listen(6900);
+app.listen(6900);
