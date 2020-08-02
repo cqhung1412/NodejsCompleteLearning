ebf7f4d668d7540924af6eecf06936b4571f5d86
Bear <wokhug99@gmail.com>
2020-08-02 20:08:46 +0700

    60. Adding Middleware

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+// const routes = require('./routes');
+
+const app = express()
+
+app.use((req, res, next) => {
+  console.log('Inside middleware 1');
+  res.write(`
+    <html>
+      <head></head>
+  `);
+  next(); // Allows the req come to below middleware
+});
+
+app.use((req, res, next) => {
+  console.log('Inside middleware 2');
+  res.write(`
+      <body>
+        <h1>Hello Expressjs</h1>
+      </body>
+    </html>
+  `);
+});
+
+const server = http.createServer(app);
 
 server.listen(6900);
-
