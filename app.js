df2dd726999d527f21b8f3b110f95fc8064be5d6
Bear <wokhug99@gmail.com>
2020-08-02 21:36:21 +0700

    64. Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use('/',(req, res, next) => {
-  console.log('This always runs!!!');
-  next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res) => {
+  res.send(`
+    <h1>Welcome to "Add Product" page!</h1>
+    <form action="/product" method="POST">
+      <input type="text" name="title"/>
+      <button type="Submit">Add New Product</button>
+    </form>
+  `);
 });
 
-app.use('/product',(req, res, next) => {
-  console.log('Inside middleware 1');
-  res.send('<h1>Welcome to "Product" page!</h1>');
-  // next(); // Cannot set headers after they are sent to the client
+app.use('/product',(req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/',(req, res, next) => {
-  console.log('Inside middleware 2');
-  res.send('<h1>Love from Express</h1>');
+  console.log(req.body);
+  res.send(`<h1>Love from Express and ${req.body.title}</h1>`);
 });
 
 app.listen(6900);
