05e1c39a74756ca3f863157b35f5f3c6ce023466
Barry Bear <wokhug99@gmail.com>
2020-10-15 00:54:50 +0700

    74. Serving Files Statically
    not just html css, also image and others

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use([adminRoutes, shopRoutes]);
 app.use('/admin', adminRoutes);
@@ -16,7 +17,7 @@ app.use(shopRoutes);
 
 // Catch all
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(6900);
