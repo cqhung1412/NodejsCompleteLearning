3dabbad88277b1b3844582aa8565b04d267bd734
Barry Bear <wokhug99@gmail.com>
2020-10-16 01:38:00 +0700

    80. Installing & Implementing Pug

--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,9 @@ const adminData = require('./admin');
 const Router = express.Router();
 
 Router.get('/',(req, res) => {
-  console.log(adminData.products); // shared across all users -> we don't want this behavior
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log(adminData.products); // shared across all users -> we don't want this behavior
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = Router;
