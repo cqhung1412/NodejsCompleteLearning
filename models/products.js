7618ba15700b86fff1d75b5a3c5d87421df0dacc
Bear <wokhug99@gmail.com>
2020-10-16 17:18:13 +0700

    99. Adding a Product Model

---
+++ models/products.js
@@ -0,0 +1,11 @@
+const products = [];
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() { products.push(this); }
+
+  static fetchAll() { return products; }
+};
