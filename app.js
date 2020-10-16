2b9cd175c57566fde584ab929feb8331568e2136
Bear <wokhug99@gmail.com>
2020-10-16 15:21:02 +0700

    84. Adding a Layout

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(shopRoutes);
 // Catch all
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404');
+    res.status(404).render('404', { docTitle: 'Page Not Found' });
 })
 
 app.listen(6900);
