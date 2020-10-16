1226d41cb074fb61e52d10a0f7beecd7ee472168
Bear <wokhug99@gmail.com>
2020-10-16 17:02:54 +0700

    404 controller

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+const errorController = require('./controllers/errors');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views'); // default
 
@@ -19,9 +21,6 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // Catch all
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-})
+app.use(errorController.get404)
 
 app.listen(6900);
