6d06ac237ac19404d9949f627e6e928d12246b96
Bear <wokhug99@gmail.com>
2020-10-29 20:54:55 +0700

    121. Passing Data with POST Requests

--- models/productsInCart.js
+++ models/productsInCart.js
@@ -4,9 +4,8 @@ const path = require('path');
 const rootDir = require('../util/path');
 const Product = require('./products');
 const productInCartDataPath = path.join(rootDir, 'data', 'productsInCart.json');
-const allProductDataPath = path.join(rootDir, 'data', 'products.json');
 
-const getProductIdsFromFile = callback => {
+const getCartProductsFromFile = callback => {
     fs.readFile(productInCartDataPath, (error, fileContent) => {
         if (error) {
             console.log(error);
@@ -24,26 +23,36 @@ module.exports = class ProductInCart {
     }
 
     save() {
-        getProductIdsFromFile(productIds => {
-            productIds.push(this);
-            fs.writeFile(
-                productInCartDataPath,
-                JSON.stringify(productIds),
-                error => { error && console.log(error); }
-            );
+        getCartProductsFromFile(cartProducts => {
+            if (!cartProducts.find(product => product.id === this.id)) {
+                cartProducts.push(this);
+                fs.writeFile(
+                    productInCartDataPath,
+                    JSON.stringify(cartProducts),
+                    error => { error && console.log(error); }
+                );
+            }
+            else {
+                console.log("Product is in cart");
+            }
         });
     }
 
     static fetchAll(cb) {
-        getProductIdsFromFile(productIds => {
-            Product.fetchAll(products => {
-                const productsInCart = [];
-                productIds.forEach(prodId => {
-                    const productById = products.find(prod => prod.id === prodId.id);
-                    productsInCart.push(productById);
+        getCartProductsFromFile(cartProducts => {
+            if (cartProducts.length !== 0) {
+                Product.fetchAll(products => {
+                    const productsInCart = [];
+                    cartProducts.forEach(cartProd => {
+                        const productById = products.find(prod => prod.id === cartProd.id);
+                        productsInCart.push(productById);
+                    });
                     cb(productsInCart);
                 });
-            });
+            }
+            else {
+                cb([]);
+            }
         });
     }
 }
--- routes/customer.js
+++ routes/customer.js
@@ -4,18 +4,24 @@ const productController = require('../controllers/customer/customer-product')
 
 const Router = express.Router();
 
+// Landing page
 Router.get('/', productController.getIndex);
 
+// Product page
 Router.get('/products', productController.getProducts);
 
+// Product detail page
 Router.get('/products/:productId', productController.getProductDetail);
 
+// Cart page
 Router.get('/cart', productController.getCart);
 
 Router.post('/cart', productController.postCart);
 
+// Checkout page
 Router.get('/checkout', productController.getCheckout);
 
+// Order page
 Router.get('/orders', productController.getOrders);
 
 module.exports = Router;
