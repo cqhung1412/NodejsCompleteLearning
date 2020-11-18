26c325b7f8586190ee1ad7de069e491c26a08b85
Barry Bear <wokhug99@gmail.com>
2020-11-19 00:03:21 +0700

    setup and install mongodb driver

--- app.js
+++ app.js
@@ -4,87 +4,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/errors');
-const sequelize = require('./util/database');
 
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
+const mongoConnect = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views'); // default
 
-const adminRoutes = require('./routes/admin');
-const customerRoutes = require('./routes/customer');
-const CartItem = require('./models/cartItem');
-const Order = require('./models/order');
-const OrderItem = require('./models/orderItem');
+// const adminRoutes = require('./routes/admin');
+// const customerRoutes = require('./routes/customer');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(errr));
+    // User.findByPk(1)
+    //     .then(user => {
+    //         req.user = user;
+    //         next();
+    //     })
+    //     .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
-app.use(customerRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(customerRoutes);
 
 // Catch all
 app.use(errorController.get404)
 
-// Admin create products
-Product.belongsTo(User, {
-    constraints: true,
-    onDelete: 'CASCADE'
-});
-User.hasMany(Product);
-// User has 1 cart
-User.hasOne(Cart);
-Cart.belongsTo(User);
-// A cart has many products/A product belongs to many carts
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-// User has many orders
-User.hasMany(Order);
-Order.belongsTo(User);
-// An order has many products/A product belongs to many orders
-Order.belongsToMany(Product, { through: OrderItem });
-// Product.belongsToMany(Order, { through: OrderItem });
-
-// { force: true }
-let MrAdmin;
-sequelize.sync()
-    .then(result => User.findByPk(1))
-    .then(user => {
-        if (!user) {
-            return User.create({
-                id: 1,
-                email: 'cqhung@node.com',
-                password: Math.round(Date.now() + Math.random()) + '',
-                role: 'ADMIN',
-                name: 'Bear Barry'
-            });
-        }
-        else {
-            return user;
-        }
-    })
-    .then(admin => {
-        MrAdmin = admin; 
-        return admin.getCart();
-    })
-    .then(cart => {
-        if (!cart) {
-            MrAdmin.createCart();
-        }
-        app.listen(6900);
-    })
-    .catch(err => console.log(err));
-
+mongoConnect((client) => {
+    console.log(client);
+    app.listen(6900);
+})
--- util/database.js
+++ util/database.js
@@ -1,13 +1,25 @@
-const { Sequelize } = require('sequelize');
+const MongoClient = require('mongodb').MongoClient;
 
-const sequelize = new Sequelize(
-  'node_complete',
-  'root',
-  '1234',
-  {
-    dialect: 'mysql',
-    host: 'localhost'
-  }
-);
+const user = 'cqhung1412';
+const password = '9Jj7pemPcodC7ULy';
+const dbname = 'shop';
 
-module.exports = sequelize;
+const uri = `mongodb+srv://${user}:${password}@nodejs-cluster0.pvske.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+const mongoConnect = callback => {
+  const client = new MongoClient(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  });
+  client.connect()
+    .then(client => {
+      callback(client);
+    })
+    .catch(err => {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+      client.close();
+    })
+}
+
+module.exports = mongoConnect;
