f05c3fd535293b043c71224b7778f625eb565af8
Bear <wokhug99@gmail.com>
2020-12-06 16:29:51 +0700

    using mongodb to store sessions

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/errors');
 
 const User = require('./models/user');
-
-const app = express();
 
 const user = 'cqhung1412';
 const password = 'jaVFccX3gHd47Qh4';
 const dbname = 'shop';
 
 const uri = `mongodb+srv://${user}:${password}@nodejs-cluster0.pvske.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+const app = express();
+const store = new MongoDBStore({
+    uri: uri,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views'); // default
@@ -29,7 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'asd24HSOIr3hid',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }));
 
 app.use((req, res, next) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 exports.getLogin = (req, res) => {
-    const isLoggedIn = req.get('Cookie')
-        .split(';')[1]
-        .trim()
-        .split('=')[1];
+    // const isLoggedIn = req.get('Cookie')
+    //     .split(';')[1]
+    //     .trim()
+    //     .split('=')[1];
+    console.log(req.session);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -14,6 +15,7 @@ exports.postLogin = (req, res) => {
     const { email, password } = req.body;
     // validate
     // ...
-    res.setHeader('Set-Cookie', 'isLoggedIn=true');
+    req.session.isLoggedIn = true;
+    // res.setHeader('Set-Cookie', 'isLoggedIn=true');
     res.redirect('/');
 };
