38b17e20a03edafd099379d691946445d0d5f467
Barry Bear <wokhug99@gmail.com>
2020-12-08 04:10:45 +0700

    adding flash to route protection

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
     const { isLoggedIn } = req.session;
     if (!isLoggedIn) {
+        req.flash('error', 'Log in first, dude :D')
         return res.redirect('/login');
     }
     next();
