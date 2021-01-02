a73b3d0d69a613029d8ac67f2e73a1c0553e102a
Barry Bear <wokhug99@gmail.com>
2021-01-03 02:31:45 +0700

    setup

--- controllers/errors.js
+++
@@ -1,8 +0,0 @@
-exports.get404 = (req, res) => {
-  const { isLoggedIn } = req.session;
-  res.status(404).render('404', { 
-      pageTitle: 'Page Not Found', 
-      path: '/404',
-      isAuth: isLoggedIn
-  });
-};
--- middleware/is-auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    const { isLoggedIn } = req.session;
-    if (!isLoggedIn) {
-        req.flash('error', 'Log in first, dude :D')
-        return res.redirect('/login');
-    }
-    next();
-};
