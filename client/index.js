57e50a5835929421804649f9245c057a5bf2a818
Barry Bear <wokhug99@gmail.com>
2021-01-03 03:21:24 +0700

    365. Sending POST Requests

--- client/index.js
+++ client/index.js
@@ -7,3 +7,17 @@ getButton.addEventListener('click', () => {
     .then(resData => console.log(resData))
     .catch(console.error);
 });
+
+postButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/post', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'A Client Post',
+      content: 'The post created by a client!'
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(res => res.json())
+    .then(resData => console.log(resData))
+    .catch(console.error);
+});
