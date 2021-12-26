78c1e3ae8f97d49c74d09a8efc81e0203b0bb1dd
hungcq3 <hungcq3@vng.com.vn>
2021-12-27 01:17:54 +0700

    using stubs

--- test/auth.js
+++ test/auth.js
@@ -1,7 +1,8 @@
+const jwt = require('jsonwebtoken');
 const { expect } = require('chai');
-const jsonwebtoken = require('jsonwebtoken');
 const { it, describe } = require('mocha');
 const authMiddleware = require('../util/is-auth');
+const Sinon = require('sinon');
 
 describe('Auth Middleware', function () {
   it('Should throw an error if there is no authorization header is present', function () {
@@ -16,18 +17,27 @@ describe('Auth Middleware', function () {
       .to.throw();
   });
 
+  it('Should yield a userId after decoding the token', function () {
+    // const token = jwt.sign({ userId: 'someUserId' }, 'asecretprivatekey');
+    // const req = { get: headerName => `Bearer ${token}` };
+    // jwt.verify = () => ({ userId: 'someUserId' });
+    const req = { get: headerName => `Bearer dasdadaasasdasdsawqe` };
+    Sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'someUserId' });
+    authMiddleware(req, {}, () => { });
+    expect(req)
+      .to.have.property('userId');
+    expect(req)
+      .to.have.property('userId', 'someUserId');
+    expect(jwt.verify.called)
+      .to.be.true;
+    jwt.verify.restore();
+  });
+
   it('Should throw an error if the token cannot be verified', function () {
     const req = { get: headerName => 'Bearer token' };
     expect(authMiddleware.bind(this, req, {}, () => { }))
       .to.throw('jwt malformed');
-  });
-
-  it('Should yield a userId after decoding the token', async function () {
-    const token = jsonwebtoken.sign({ userId: 'someUserId' }, 'asecretprivatekey');
-    const req = { get: headerName => `Bearer ${token}` };
-    authMiddleware(req, {}, () => { });
-    expect(req)
-      .to.have.property('userId');
   });
 });
 
